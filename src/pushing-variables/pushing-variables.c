8aa6d858eaa1ab58c1f2fb242ddfc61249467ce9
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-09-30 17:23:23 -0300

    Resolve a TODO: example for instance vars

--- src/pushing-variables/pushing-variables.c
+++ src/pushing-variables/pushing-variables.c
@@ -5,6 +5,17 @@
 #include <mruby/variable.h>
 #include <mruby/value.h>
 
+/* Simply set an integer as ivar */
+static mrb_value mrb_foobar_new(mrb_state* mrb, mrb_value self) {
+    mrb_int num;
+
+    mrb_get_args(mrb, "i", &num);
+
+    mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "@num"), mrb_fixnum_value(num));
+
+    return self;
+}
+
 /* This sample program shows you how to push various tipes of
  * variables to ruby
  * TODO: Get the variables and print them */
@@ -24,10 +35,19 @@ int main(int argc, char* argv[]) {
                mrb_intern_lit(mrb, "$bar"),
                mrb_str_new_lit(mrb, "I'm a global!"));
 
-    // TODO: Instance variables
-
-    // Class variables
+    // Instance variables
     struct RClass* foobarKlass = mrb_define_class(mrb, "Foobar", mrb->object_class);
+    mrb_define_method(mrb, foobarKlass, "initialize", mrb_foobar_new, MRB_ARGS_REQ(1));
+
+    // Create the object
+    mrb_value args[1] = { mrb_fixnum_value(1500) };
+    mrb_value obj = mrb_obj_new(mrb, foobarKlass, 1, args);
+
+    // Get the ivar
+    mrb_int numIvar = mrb_fixnum(mrb_iv_get(mrb, obj, mrb_intern_lit(mrb, "@num")));
+    printf("@num: %ld\n", numIvar);
+
+    // Class variables (We'll use the same class as above)
     mrb_mod_cv_set(mrb,
                    foobarKlass,
                    mrb_intern_lit(mrb, "@@baz"),
@@ -43,11 +63,11 @@ int main(int argc, char* argv[]) {
 
     // We can also get the variables we defined, for example; getting the
     // module variable @@quux
-    mrb_value quuxCvar = mrb_cv_get(mrb,
-                                     mrb_obj_value(foobazModule),
-                                     mrb_intern_lit(mrb, "@@quux"));
+    mrb_float quuxCvar = mrb_float(mrb_cv_get(mrb,
+                                   mrb_obj_value(foobazModule),
+                                   mrb_intern_lit(mrb, "@@quux")));
 
-    printf("@@quux: %f\n", mrb_float(quuxCvar));
+    printf("@@quux: %f\n", quuxCvar);
 
     mrb_close(mrb);
     return 0;
