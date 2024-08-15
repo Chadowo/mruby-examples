ced474a024c6f8dd402dad12ca6771b12ee5dc35
Chadowo <83732118+Chadowo@users.noreply.github.com>
2024-08-14 22:39:28 -0300

    Tweak example and rename

--- src/method-defined/method-defined.c
+++ src/method-defined/method-defined.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #include <mruby.h>
 #include <mruby/value.h>
 
-/* This function will perform a search in the Ruby side for the method that
-   you that'd specify it to. A return value of 1 means that it was found, 0
-   the opposite */
-static int check_method(mrb_state* mrb, char* method_name) {
+/* Check if a certain method is defined using respond_to? */
+static bool method_defined(mrb_state* mrb, char* method_name) {
     // Create a symbol corresponding to the method name
     mrb_sym method_sym = mrb_intern_cstr(mrb, method_name);
 
-    // Either 1 (true) or 0 (false)
     return mrb_obj_respond_to(mrb, mrb->object_class, method_sym);
 }
 
@@ -35,7 +33,7 @@ int main(int argc, char* argv[]) {
                       MRB_ARGS_NONE());
 
     // Checking for a method
-    if(check_method(mrb, "example_method")) {
+    if(method_defined(mrb, "example_method")) {
       printf("'example_method' found!\n");
     }
 
